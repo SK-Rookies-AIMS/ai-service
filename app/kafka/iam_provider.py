ce664a7c47262a72ae4c222d66e261e062ec935c
kimgeon <kimgeon0802@gmail.com>
2026-06-26 15:18:48 +0900

    fix : iam_provider.py update

--- app/kafka/iam_provider.py
+++ app/kafka/iam_provider.py
@@ -2,11 +2,7 @@ from kafka.sasl.oauth import AbstractTokenProvider
 from aws_msk_iam_sasl_signer import MSKAuthTokenProvider
 
 
-class MSKTokenProvider:
-
-    def token(self) -> str:
-        token, _ = MSKAuthTokenProvider.generate_auth_token(
-            region="ap-northeast-2",
-        )
-
+class MSKTokenProvider(AbstractTokenProvider):
+    def token(self):
+        token, _ = MSKAuthTokenProvider.generate_auth_token("ap-northeast-2")
         return token
