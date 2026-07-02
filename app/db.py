208d1164e49cb6d0eb44639c5bd8c10eb4479c16
kimgeon <kimgeon0802@gmail.com>
2026-07-02 14:45:25 +0900

    fix : DB session out code update 2

--- app/db.py
+++ app/db.py
@@ -4,13 +4,25 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 load_dotenv()
 
 # =========================
 # DATABASE URL
 # =========================
-DATABASE_URL = os.getenv("MAIN_DATABASE_URL")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = quote_plus(
+    os.getenv("DB_PASSWORD")
+)
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+MAIN_DB_NAME = os.getenv("MAIN_DB_NAME")
+
+DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{MAIN_DB_NAME}"
+)
 
 if not DATABASE_URL:
     raise Exception("MAIN_DATABASE_URL is not set")
