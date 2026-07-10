d58af024202f2172a8e16b3f0bcdeb64ecb2ae81
kimgeon <kimgeon0802@gmail.com>
2026-07-10 17:13:40 +0900

    fix : env del

--- app/db.py
+++ app/db.py
@@ -1,48 +1,11 @@
 # app/db.py
 
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from dotenv import load_dotenv
-from urllib.parse import quote_plus
+from app.core.config import settings
 
-load_dotenv()
-
-# =========================
-# DATABASE URL
-# =========================
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = quote_plus(
-    os.getenv("DB_PASSWORD")
-)
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-MAIN_DB_NAME = os.getenv("MAIN_DB_NAME")
-SAMPLE_DB_NAME = os.getenv("SAMPLE_DB_NAME")
-
-MAIN_DATABASE_URL = (
-    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_HOST}:{DB_PORT}/{MAIN_DB_NAME}"
-)
-
-SAMPLE_DATABASE_URL = (
-    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_HOST}:{DB_PORT}/{SAMPLE_DB_NAME}"
-)
-
-if not MAIN_DATABASE_URL:
-    raise Exception("MAIN_DATABASE_URL is not set")
-
-if not SAMPLE_DATABASE_URL:
-    raise Exception("SAMPLE_DATABASE_URL is not set")
-
-# =========================
-# ENGINE (SINGLETON)
-# =========================
 main_engine = create_engine(
-    MAIN_DATABASE_URL,
-    echo=False,
-
+    settings.main_database_connection_url,
     # ===== connection pool =====
     pool_size=10,            # 기본 유지 커넥션
     max_overflow=20,         # 추가 커넥션 허용
@@ -55,7 +18,7 @@ main_engine = create_engine(
 )
 
 sample_engine = create_engine(
-    SAMPLE_DATABASE_URL,
+    settings.sample_database_connection_url,
     echo=False,
 
     # ===== connection pool =====
@@ -68,7 +31,6 @@ sample_engine = create_engine(
     # (옵션) 멀티스레드 안정성
     # connect_args={"check_same_thread": False}  # SQLite일 때만
 )
-
 
 # =========================
 # SESSION FACTORY
