078d9a9ebded299036468c6603466f3862ab3c7e
mmije0ng <victon777@naver.com>
2026-07-21 13:53:58 +0900

    chore: git action trigger

--- app/dto/response/common_response.py
+++ app/dto/response/common_response.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel, Field
 
 DataT = TypeVar("DataT")
 
-
 class CommonResponse(BaseModel, Generic[DataT]):
     success: bool = Field(description="요청 성공 여부")
     data: DataT | None = Field(default=None, description="응답 데이터")
